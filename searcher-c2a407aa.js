27c436414e5d7a774b31b83f830831daed8c7bdb
Mohith3600 <Mohith3600@users.noreply.github.com>
2026-04-28 03:58:42 +0000

    deploy: 2a2cedd20f08e76e38e1f1a53abde1d3b7263b44

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d5f7e573.js',
+                path_to_root + 'searchindex-b6900690.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
